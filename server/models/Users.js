0db683cd7524ee8616d5257d4e1d706d44e6379a
Paulina Chabrand <paulinachabrand@MacBook-Pro-de-Paulina.local>
2020-02-10 21:26:49 -0600

    modelos

---
+++ server/models/Users.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+// creamos un nuevo objeto
+const UserSchema = new Schema ({
+	first_name:{
+		type:String,
+		required:true
+	},
+	last_name:{
+		type:String,
+		required:true
+	},
+	email:{
+		type:String,
+		required:true,
+		unique:true
+	},
+	password:{
+		type:String
+	},
+	birth_date:{
+		type:Date
+	},
+	gender:{
+		type:String,
+		enum:['M','F','O']
+	},
+	is_active:{
+		type:Boolean,
+		default:true
+	}
+
+},{timestamps:true});
+
+UserSchema.pre('save',function(next){
+	const user = this;
+	const SALT_FACTOR = 10;
+	if(!user.isModified('password')) next();
+	bcrypt.genSalt(SALT_FACTOR, function(err, salt){
+		if(err) return next(err);
+
+		bcrypt.hash(user.password, salt, function(error, hash){
+			user.password = hash;
+			next();
+		});
+	});
+
+});
+
+module.exports = mongoose.model('users',UserSchema);
